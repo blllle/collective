0ac32b6c7de6693d172b2d6e6d4c1148d85be791
Billie Gray <billiexgray@gmail.com>
2019-01-29 00:38:48 +0100

    Js for slide menu and sticky on scroll

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.menu-toggle').on('click', function () {
+
+
+    $('.offscreen').toggleClass('open');
+    $('body').toggleClass('menu-open');
+
+
+
+  });
+
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 600) {
+      $(".header").addClass("active");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header").removeClass("active");
+    }
+  });
+
+
+});
