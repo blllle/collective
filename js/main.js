bec1ec3a0cc5c6e5e2a9e169ffc972aa9be06b59
Billie Gray <billiexgray@gmail.com>
2019-01-30 16:34:27 +0100

    variables for colour, updated menu state to change colour on scrolling, added new section CSS for icon block example

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(function () {
   $('.menu-toggle').on('click', function () {
 
-
     $('.offscreen').toggleClass('open');
     $('body').toggleClass('menu-open');
-
-
 
   });
 
@@ -13,9 +10,11 @@ $(function () {
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 600) {
       $(".header").addClass("active");
+      $(".menu-toggle-svg").addClass("scrolled");
     } else {
       //remove the background property so it comes transparent again (defined in your css)
       $(".header").removeClass("active");
+      $(".menu-toggle-svg").removeClass("scrolled");
     }
   });
 
